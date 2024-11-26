81b8020b8085e874ffe6f1df5bfbbf25003e98d2
manish.podugu <manish.podugu@triniti.com>
2024-11-26 12:03:49 +0100

    added testcases to solve

---
+++ unit_test_app.py
@@ -0,0 +1,10 @@
+import pytest
+from app import add, subtract
+
+def test_add():
+    assert add(3, 5) == 8
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(10, 5) == 5
+    assert subtract(0, 5) == -5
